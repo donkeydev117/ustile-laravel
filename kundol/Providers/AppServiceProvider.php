e2bfcefca828752db0f731e9760f5c248d9c92eb
Sr.React Native Developer <samcheng955@gmail.com>
2021-12-02 07:48:11 -0800

    Update the passport problem

--- kundol/Providers/AppServiceProvider.php
+++ kundol/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Passport\Passport;
+use Laravel\Passport\Console\ClientCommand;
+use Laravel\Passport\Console\InstallCommand;
+use Laravel\Passport\Console\KeysCommand;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,5 +30,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Passport::routes();
+
+        /*ADD THIS LINES*/
+        $this->commands([
+            InstallCommand::class,
+            ClientCommand::class,
+            KeysCommand::class,
+        ]);
     }
 }
