b232239c98bbedb1d5b48466ff6ab090f2c157f6
rndeveloper <samcheng955@gmail.com>
2022-04-05 10:52:20 -0400

    Update the header menu

--- resources/views/includes/headers/main-menu.blade.php
+++ resources/views/includes/headers/main-menu.blade.php
@@ -1,5 +1,77 @@
 
       <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://127.0.0.1:8000/shop?category=#">Title Collection</a>
+          <div class="dropdown-menu">
+            <div class="dropdown-submenu submenu0">
+              <a class="dropdown-item" href="http://127.0.0.1:8000/shop?category=1">
+                <figure class="category-image">
+                  <div class="hex2"></div>
+                  <div class="hex">
+                      <img src="/gallary/202112104256FOREST_LIANA.jpeg" />
+                  </div>
+                </figure>
+                <div class="category-title">
+                  <img src="/images/dropdown-menu-arrow-icon.png" />
+                  <h4 class="text-center pt-2">Category 1</h4>
+                </div>
+              </a>
+              <a class="dropdown-item" href="http://127.0.0.1:8000/shop?category=4">
+                <figure class="category-image">
+                  <div class="hex2"></div>
+                  <div class="hex">
+                      <img src="/gallary/202112103948ONE_COAL.jpeg" />
+                  </div>
+                </figure>
+                <div class="category-title">
+                  <img src="/images/dropdown-menu-arrow-icon.png" />
+                  <h4 class="text-center pt-2">Category 2</h4>
+                </div>
+              </a>
+              <a class="dropdown-item" href="http://127.0.0.1:8000/shop?category=2">
+                <figure class="category-image">
+                  <div class="hex2"></div>
+                  <div class="hex">
+                    <img src="/gallary/202112104256FOREST_LIANA.jpeg" />
+                  </div>
+                </figure>
+                <div class="category-title">
+                  <img src="/images/dropdown-menu-arrow-icon.png" />
+                  <h4 class="text-center pt-2">Category 3</h4>
+                </div>
+              </a>
+              <a class="dropdown-item" href="http://127.0.0.1:8000/shop?category=5">
+                <figure class="category-image">
+                  <div class="hex2"></div>
+                  <div class="hex">
+                    <img src="/gallary/202112103948ONE_COAL.jpeg" />
+                  </div>
+                </figure>
+                <div class="category-title">
+                  <img src="/images/dropdown-menu-arrow-icon.png" />
+                  <h4 class="text-center pt-2">Category 4</h4>
+                </div>
+              </a>
+              <a class="dropdown-item" href="http://127.0.0.1:8000/shop?category=5">
+                <figure class="category-image">
+                  <div class="hex2"></div>
+                  <div class="hex">
+                    <img src="/gallary/202112103948ONE_COAL.jpeg" />
+                  </div>
+                </figure>
+                <div class="category-title">
+                  <img src="/images/dropdown-menu-arrow-icon.png" />
+                  <h4 class="text-center pt-2">Category 5</h4>
+                </div>
+              </a>
+            </div>
+            <div class="dropdown-all-collection-container">
+              <div>
+                <a href="#">View All Collections</a>
+              </div>
+            </div>
+          </div>                  
+        </li>
         @php  $temp = '0' @endphp
         
         @if(isset($header_menu->menu))
